56ade3e217ca5cab385899fda7f390cad6423b81
mmdev110 <mmdev8787@gmail.com>
2023-08-28 00:36:02 +0000

    add addAuthorizationHeader util

--- app/handlers/setup_test.go
+++ app/handlers/setup_test.go
@@ -16,6 +16,8 @@ import (
 )
 
 var expiredToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjEwMCwidG9rZW5fdHlwZSI6ImFjY2VzcyIsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6NTAwMCIsImF1ZCI6WyJodHRwOi8vbG9jYWxob3N0OjMwMDAiXSwiZXhwIjoxNjkyNTA4ODc0LCJuYmYiOjE2OTI1MDg4MTQsImlhdCI6MTY5MjUwODgxNH0.Nw5g5FYh_uiZkvOg0bhxV0nIP_Z75lYZ72xjwOArbL0"
+
+// テスト用のサーバー
 var ts *httptest.Server
 
 var h = HandlersConf{
@@ -107,4 +109,13 @@ func template(t *testing.T) {
 		})
 	}
 
+}
+
+func addAuthorizationHeader(req *http.Request, user *models.User) error {
+	authorization, err := user.GenerateToken("access")
+	if err != nil {
+		return err
+	}
+	req.Header.Add("Authorization", "Bearer "+authorization)
+	return nil
 }
