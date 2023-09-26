8743911675796a91989acdd0ee80b1337ab0387e
mmdev110 <mmdev8787@gmail.com>
2023-09-26 03:11:42 +0000

    remove log

--- app/auth/setup_test.go
+++ app/auth/setup_test.go
@@ -9,11 +9,9 @@ import (
 var auth = Auth{}
 
 func TestMain(m *testing.M) {
-	fmt.Println("@@@@@@@@@")
 	region := os.Getenv("AWS_REGION")
 	userPoolID := os.Getenv("AWS_COGNITO_USER_POOL_ID")
 	appClientID := os.Getenv("AWS_COGNITO_APP_CLIENT_ID")
-	fmt.Println(userPoolID)
 	if err := auth.Configure(region, userPoolID, appClientID); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
